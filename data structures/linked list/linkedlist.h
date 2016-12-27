edd2ba6e4714d1ab198fb4457f52eb7e8d2f2e8e
Avinash Bondalapati <avinashbondalapati@gmail.com>
2016-12-27 22:16:25 +0530

    Update linkedlist.h

--- data structures/linked list/linkedlist.h
+++ data structures/linked list/linkedlist.h
@@ -4,12 +4,6 @@ typedef struct LinkedList{
 	struct LinkedList *next;
 }Node;
 
-//all the methods signatures
-bool pair_with_sum_k(int *values, int n, int sum);
-bool tuple_with_sum_k(int *values, int n, int sum);
-int comparator(const void *a, const void *b);//util method used for the default qsort.
-int number_repeated_odd_number_of_times(int *values, int n);
-
 //linked list methods signatures
 Node *createLinkedList();
 Node *createNode(int data);
