b29d639f736086b35fb46874bd62f997b55c42a2
avinash625 <avinashbondalapati@gmail.com>
2017-07-03 01:28:53 +0530

    initial draft for the competitive programming library

---
+++ drafts/initcpp.cpp
@@ -0,0 +1,36 @@
+/*
+	"author"	:	"avinash bondalapati"
+	"date"		:	"02-07-2017"
+	"language"	:	"c++"
+
+*/
+
+#include<iostream>
+#include<stdlib.h>
+#include<memory.h>
+#include<limits.h>
+#include<cstdint.h>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<algorithm>
+
+#define first F
+#define second S
+#define (long long int) ll
+#define long int l
+#define long double d
+#define mod 1e10+7
+#define log(text) cout<<"log:"<<" "<<text<<endl
+#define for(n) for(int iter = 0;iter<n;iter++)
+#define for(1,n) for(int iter = 1 ;iter<n;iter++)
+#define for(n,0) for(int iter = n-1;iter>=0;iter--)
+#define clear(a) memset((a),0,sizeof(a))
+#define read(a,n) for(int iter = 0;iter< n;iter++){cin>>a[iter];}
+#define print(a) for(int iter = 0;iter<(a).size;i++ ){ cout<<a[iter]<<" ";}
