bf6eb20d2bf2cf50ef26f0f98712577ff6b5e0cb
Avinash Bondalapati <avinashbondalapati@gmail.com>
2017-09-10 22:24:54 +0530

    Create Fizz_Buzz.py

---
+++ LeetCode/Fizz_Buzz.py
@@ -0,0 +1,20 @@
+#Problem Link:
+#https://leetcode.com/problems/fizz-buzz/description/
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        for x in xrange(1,n+1,1):
+            if x%3 == 0 and x%5 ==0 :
+                result.append("FizzBuzz")
+            elif x%3 == 0:
+                result.append("Fizz")
+            elif x%5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(x))
+        return result
+        
