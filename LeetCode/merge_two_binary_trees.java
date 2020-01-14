cd0a07909e02f7ffff58fa0d5bff79fbc9d0bf45
Avinash Bondalapati <avinashbondalapati@gmail.com>
2020-01-13 21:06:28 -0500

    added merge_two_binary_trees.java

---
+++ LeetCode/merge_two_binary_trees.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) {
+            //System.out.println("both nodes are null so returning null");
+            return t1;
+        }
+        else{
+            if(t1 != null && t2 != null){
+                //System.out.println("both nodes are NOT null with values of " + t1.val + " , " + t2.val);
+                //System.out.println("entering into left tree of "+ t1.val);
+                t1.left = mergeTrees(t1.left, t2.left);
+                //System.out.println(" entering into right tree of "+t1.val);
+                t1.right = mergeTrees(t1.right, t2.right);
+                //System.out.println("updated value with sum " + (t1.val + t2.val));
+                t1.val = t1.val + t2.val;
+                return t1;
+            }else if(t1 == null) {
+                //System.out.println("returning t2 " + t2.val);
+                return t2;
+            }
+            else{
+                //System.out.println("returning t1 " + t1.val);
+                return t1;
+            }
+        }
+    }
+}
