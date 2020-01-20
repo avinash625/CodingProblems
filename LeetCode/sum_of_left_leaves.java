0085418a67e48c2a4e6b8284919580c59c460bf8
avinash625 <avinashbondalapati@gmail.com>
2020-01-20 00:19:46 -0500

    added sum_of_left_leaves.java

---
+++ LeetCode/sum_of_left_leaves.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null) return 0;
+        else{
+            if(root.left != null && root.left.left == null && root.left.right == null){
+                return root.left.val + sumOfLeftLeaves(root.right);
+            }else{
+                return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+            }
+        }
+    }
+}
