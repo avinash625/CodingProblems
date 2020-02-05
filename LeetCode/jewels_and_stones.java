0ba3c53a072c583ece472832a65a5033b923bbfb
avinash625 <avinashbondalapati@gmail.com>
2020-02-04 23:06:53 -0500

    added jewels_and_stones.java

---
+++ LeetCode/jewels_and_stones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        HashMap<Character,Integer> map = new HashMap<Character, Integer>();
+        for( int index = 0; index < S.length(); index++){
+            if(map.containsKey(S.charAt(index)) == true){
+                map.put(S.charAt(index) ,map.get(S.charAt(index)) + 1);
+            }else{
+                map.put(S.charAt(index),1);
+            }
+        }
+        int jewels = 0;
+        for(int index = 0; index < J.length(); index++){
+            if(map.containsKey(J.charAt(index)))
+                jewels = jewels + map.get(J.charAt(index));
+        }
+        return jewels;
+    }
+}
