6a8a4559e4039f93425781d8e0423b64a21ea764
avinash625 <avinashbondalapati@gmail.com>
2020-02-14 01:53:56 -0500

    added maximum_product_of_three_numbers.java

---
+++ LeetCode/maximum_product_of_three_numbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        
+        int length = nums.length;
+        
+        if(nums[length-1] * nums[length-2]* nums[length-3] > nums[0]*nums[1] * nums[length-1]){
+            return nums[length-1] * nums[length-2]* nums[length-3];
+        }else{
+            return nums[0]*nums[1] * nums[length-1];
+        }
+    }
+}
